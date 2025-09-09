d576d9f03c3af07162bb2c0bfd3b95b80fdca6d9
Muneeb Zahoor Git <zahoormuneeb714@gmail.com>
2025-09-09 19:56:46 +0500

    inital cli table3 and create 32_weather_app cli weather app

--- NodeJS_notes/projects/33_37_url_shortener/app.js
+++ NodeJS_notes/projects/33_37_url_shortener/app.js
@@ -1,35 +1,92 @@
 import { createServer } from 'http';
 import fs from 'fs';
 import path from 'path';
+import { parse } from "querystring"
+import chalk from 'chalk';
+import { json } from 'stream/consumers';
+
 let port = 3000;
 
-
-async function serveFile(req, res, filePath, contentType) {
+//! Server File Function :)
+async function serveFile(req, res, filePath, contentType, statusCode) {
     try {
         const data = await fs.promises.readFile(filePath, 'utf-8');
-        res.writeHead(200, { 'Content-Type': contentType });
+        res.writeHead(statusCode, { 'Content-Type': contentType });
         res.end(data);
     } catch (err) {
-        res.writeHead(404, { 'Content-Type': contentType });
+        res.writeHead(statusCode, { 'Content-Type': contentType });
         res.end('Page not found');
     }
-    console.log("Request served for " + filePath);
-    console.log("req.url: " + req.url);
-    console.log("req.method: " + req.method);
+
+}
+
+
+//! UpdateLogs Funiton
+const updateLogs = async (req) => {
+    let now = new Date();
+    let logObj = {
+        requestURL: req.url,
+        requestMethod: req.method,
+        requsetTime: now.toLocaleTimeString(),
+        requsetDate: now.toLocaleDateString(),
+    }
+    let filename = "logs.json";
+    let filePath = path.join("data", filename);
+
+
+    try {
+        let logs = [];
+        if (fs.existsSync(filePath)) {
+            let data = await fs.promises.readFile(filePath, "utf-8");
+            if (data) {
+
+                logs = await JSON.parse(data);
+            }
+        }
+
+        await logs.push(logObj);
+
+        await fs.promises.writeFile(filePath, JSON.stringify(logs, null, 2));
+        console.log(chalk.green("Log updated: ") + chalk.yellow(logObj.requsetDate, logObj.requsetTime))
+    } catch (error) {
+        console.log(chalk.red("Error Updating logs...", error))
+    }
+
 
 }
 
 const server = createServer(async (req, res) => {
+
     if (req.method == "GET") {
         if (req.url == "/") {
-            serveFile(req, res, path.join('public', 'index.html'), 'text/html');
+            serveFile(req, res, path.join('public', 'index.html'), 'text/html', 200);
         } else if (req.url == "/style.css") {
-            serveFile(req, res, path.join('public', 'style.css'), 'text/css');
-        } else if (req.url == "/muneeb.html") {
-            serveFile(req, res, path.join('public', 'muneeb.html'), 'text/html');
+            serveFile(req, res, path.join('public', 'style.css'), 'text/css', 200);
         } else {
-            serveFile(req, res, path.join('public', '404.html'), 'text/html');
+            serveFile(req, res, path.join('public', '404.html'), 'text/html', 404);
         }
+    }
+
+    // Handle POST
+    if (req.method === "POST" && req.url === "/shorten") {
+        let body = "";
+        req.on("data", (chunk) => {
+            body += chunk;
+        });
+
+        req.on("end", () => {
+            try {
+                const formData = Object.fromEntries(
+                    body.split("&").map(pair => pair.split("=").map(decodeURIComponent))
+                );
+                //! removing &&%% Symbols form url :)
+                console.log(formData);
+                res.writeHead(200, { "Content-Type": "text/plain" });
+                res.end(`Short URL created: ${formData.shortCode} Original URL: ${formData.url}`);
+            } catch (err) {
+                serveFile(req, res, path.join('public', '404.html'), 'text/html', 404);
+            }
+        });
     }
 })
 
